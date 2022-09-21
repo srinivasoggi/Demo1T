995e6fe892e1f44591c0e0ff39c11a1766997127
srinivas oggi <sri.techsuport@gmail.com>
2022-09-21 09:59:39 +0530

    FirstCommit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		System.out.println("Hello, im printing numbers from 1 to 10");
+		for(int i=1;i<=10;i++)
+			System.out.println(i);
+		
+	}
+
+}
